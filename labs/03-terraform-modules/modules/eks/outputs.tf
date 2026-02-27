484412beff1cdda0fbd3b535ae64a5f564626b21
Vanessa Awo <vanessa93730@gmail.com>
2026-02-26 18:02:21 -0600

    Remove labs directory from main branch
    Labs content lives on the labs branch.

--- labs/03-terraform-modules/modules/eks/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "cluster_endpoint" { value = aws_eks_cluster.this.endpoint }
-output "cluster_name" { value = aws_eks_cluster.this.name }
-output "cluster_ca" { value = aws_eks_cluster.this.certificate_authority[0].data }
-output "oidc_provider_arn" { value = aws_iam_openid_connect_provider.cluster.arn }
--- labs/03-terraform-modules/modules/rds/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "cluster_endpoint" { value = aws_rds_cluster.this.endpoint }
-output "reader_endpoint" { value = aws_rds_cluster.this.reader_endpoint }
-output "cluster_id" { value = aws_rds_cluster.this.id }
